acfb3d192f6a8388697996af1e45c04d2f2add52
Spencer <spencer.morphy@live.ca>
2026-06-21 14:29:20 -0700

    mag data parser

--- scripts/magdataparser.py
+++ scripts/magdataparser.py
@@ -1,5 +1,23 @@
 ### Mag Data Parser Script
 ### Author: Spencer Morphy
 ### Date June 21, 2026
+### Parses raw mag data into a PostgreSQL db
 
+# 1 Open the file
+fd = open("../rawdata/magdataraw.txt", "r")
+for line in fd:
+    if line[0] == "/" and line[1] == "E":
+        print("Header found")
+        print(line)
+        # strip leading / and insert into pandas dataframe as header
+
+    if line[0] != "/":
+        #data line
+        print("dataline")
+        print(line)
+
+
+# 2 Parse file into PD dataframe?
+
+# 3 Create Postgres DB and insert data into?
 
