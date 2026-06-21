0a398ba43d7d6e4c7bc6eb2b23342fb93f0331bc
Spencer <spencer.morphy@live.ca>
2026-06-21 15:17:13 -0700

    finished data parsing logic

--- scripts/magdataparser.py
+++ scripts/magdataparser.py
@@ -12,24 +12,26 @@ metadata_lines = []
 
 fd = open("../rawdata/magdataraw.txt", "r")
 for line in fd:
+    # header
     if line[0] == "/" and line[1] == "E":
-        print("Header found")
         header = line.lstrip("/").split()
-        print(header)
-
+    
+    #line id
     if line[0] == "/" and line[1] == "l":
-        print("Line number updated")
         full_line = line.lstrip("/").split()
         current_line_id = full_line[1]
-        print(current_line_id)
-
+   
+   #data line
     if line[0] != "/":
         #data line
-        print("dataline")
-        print(line)
+        data_line = line.split()
+        row_dict = dict(zip(header, data_line))
+        row_dict["line_id"] = current_line_id
+        data_rows.append(row_dict)
 
 
 # 2 Parse file into PD dataframe?
+# df = pd.DataFrame(data_rows)
 
 # 3 Create Postgres DB and insert data into?
 
